a335deb7861f18d564449a375d15778737168417
amit596 <kaliraman449@gmail.com>
2025-09-14 10:54:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -424,7 +424,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         user_link = f"https://t.me/{user.username}" if user.username else f"tg://openmessage?user_id={OWNER_ID}" 
         ownername = f"<a href={user_link}>{user.first_name}</a>" if user.first_name else f"<a href={user_link}>no name !</a>"
         await query.edit_message_media(
-            InputMediaPhoto("https://envs.sh/Wdj.jpg", 
+            InputMediaPhoto("https://envs.sh/iaV.jpg", 
                             CHANNELS_TXT
             ),
             reply_markup=InlineKeyboardMarkup([
